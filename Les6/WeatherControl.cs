f296197c9cfb7de1cb5d2911cf2d3835fa9e2415
Zoia611 <117438266+Zoia611@users.noreply.github.com>
2023-01-03 19:05:55 +0300

    Добавьте файлы проекта.

---
+++ Les6/WeatherControl.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Les6
+{
+    class WeatherControl : DependencyObject
+    {
+        public static readonly DependencyProperty TempProperty;
+        public string WindDirection { get; set; }
+        public int WindSpeed { get; set; }
+
+        enum precip
+        {
+            sun = 0,
+            cloud = 1,
+            rain = 2,
+            snow = 3,
+        }
+
+        public int Temp
+        {
+            get => (int)GetValue(TempProperty);
+            set => SetValue(TempProperty, value);
+        }
+
+
+        public WeatherControl(string windDirection, int windSpeed, int temp)
+        {
+            this.WindDirection = windDirection;
+            this.WindSpeed = windSpeed;
+            this.Temp = temp;
+        }
+
+        static WeatherControl()
+        {
+            TempProperty = DependencyProperty.Register(
+                nameof(Temp),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsMeasure |
+                    FrameworkPropertyMetadataOptions.AffectsArrange,
+                    null,
+                    new CoerceValueCallback(CoerceTemp)),
+                new ValidateValueCallback(ValidateTemp));
+        }
+
+        private static object CoerceTemp(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50 && v <= 50)
+                return v;
+            else
+                return 0;
+        }
+
+        private static bool ValidateTemp(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+                return true;
+            else
+                return false;
+        }
+    }
+}
